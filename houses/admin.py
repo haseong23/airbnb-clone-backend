7467b3c94f4b692f9de1ec505e2dd3a21c679003
Harrison(Pyo Haseong) <tempyo42@gmail.com>
2024-09-17 14:47:47 +0900

    #4.3 Admin

--- houses/admin.py
+++ houses/admin.py
@@ -4,5 +4,23 @@ from .models import House
 # Register your models here.
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    # 리스트형이거나 튜플형이어야함.
+    # 인스턴스 선택전에 보여지는 컬럼
+    list_display = (
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed",
+    )
 
+    # 필터링
+    list_filter = (
+        "price_per_night",
+        "pets_allowed",
+    )
+
+    # 검색
+    search_fields = (
+        "address__startswith",
+        "name",
+    )
--- houses/models.py
+++ houses/models.py
@@ -9,7 +9,11 @@ class House(models.Model):
     """
     # 속성
     name = models.CharField(max_length=140)
-    price_per_nigth = models.PositiveBigIntegerField()
+    price_per_night = models.PositiveBigIntegerField()
     discription = models.TextField()
     address = models.CharField(max_length=1500)
     pets_allowed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        # Object명이 아닌 House의 이름을 표시하기 위함임.
+        return self.name
