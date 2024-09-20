32b65e03c058a4b135ae4ca48984af3ba50df207
Harrison(Pyo Haseong) <tempyo42@gmail.com>
2024-09-21 00:09:50 +0900

    #5.4 Custom Admin

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class User(AbstractUser):
     # Overwrite
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
-    
+
     # custom
-    name = models.CharField(max_length=150)
-    is_host = models.BooleanField()
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
